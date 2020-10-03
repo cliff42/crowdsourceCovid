e8a44848e76adfe4ca017e39affb4c929c678f70
David Cha <djc991006@gmail.com>
2020-10-02 23:16:00 -0600

    Initial commit

---
+++ controllers/htmlController.js
@@ -0,0 +1,12 @@
+var bodyparser = require('body-parser');
+
+var urlencodedParser = bodyparser.urlencoded({ extended: false });
+var jsonParser = bodyparser.json();
+
+module.exports = function(app) {
+	
+	app.get('/', function(req, res) {
+		res.render('index');
+	});
+	
+}
